927641968fdc43dcdcb8035c6f00264e63c1e0ce
Lucas Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2026-08-19 23:34:56 -0300

    Conectar formulario con POST y refrescar tabla automáticamente

--- app.js
+++ app.js
@@ -33,4 +33,38 @@ async function cargarGastos() {
     document.getElementById("cuerpo-tabla-gastos").innerHTML += fila;
   });
 }
+
+async function guardarGasto(descripcion, monto, categoria_id, fecha) {
+  const response = await fetch("src/controllers/api.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      descripcion: descripcion,
+      monto: monto,
+      categoria_id: categoria_id,
+      fecha: fecha,
+    }),
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
+
+document
+  .getElementById("form-gasto")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const descripcion = document.getElementById("descripcion").value;
+    const monto = document.getElementById("monto").value;
+    const categoria_id = document.getElementById("categoria_id").value;
+    const fecha = document.getElementById("fecha").value;
+
+    await guardarGasto(descripcion, monto, categoria_id, fecha);
+
+    cargarGastos();
+  });
+
 cargarGastos();
